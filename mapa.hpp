439f2314ebe5a365f1065d740cc2c2bcab673efd
Uthanor <40926321+Uthanor@users.noreply.github.com>
2019-03-15 20:30:13 -0300

    This is de master branch of the game.
    This branch have a problem. This problem is that is not good for linux.
    I crated a branch that is good for linux. This branch works with ncurses.

---
+++ mapa.hpp
@@ -0,0 +1,24 @@
+#ifndef MAPA_HPP
+#define MAPA_HPP
+
+class mapa{
+private:
+	char **matriz_mapa;				//Puntero para asignar memoria que representa el mapa.
+	int x;
+	int y;
+	bool memoria;
+public:
+	mapa();
+	mapa(int width, int height);			//Constructor del mapa.
+	
+	void definir(int height, int width);		//Define dimension del mapa.
+	void erase();					//Borra y libera el mapa.
+	
+	bool setData(int i, int j, char data);		//Establece datos en el mapa.
+	char getData(int i, int j);			//Devuelve datos en el mapa
+	void setFloor(char simbol);			//Establece el simbolo del piso.
+	void mostrar();					//Imprime en pantalla el mapa del mundo.
+	~mapa();
+};
+
+#endif
